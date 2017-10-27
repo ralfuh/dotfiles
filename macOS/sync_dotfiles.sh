a2fbe84b8921b658eff19b4a7edabda537bbff1d
rf <robert.a.fuhrman@gmail.com>
2017-10-26 21:50:03 -0700

    Added .inputrc, .functions

--- macOS/sync_dotfiles.sh
+++ macOS/sync_dotfiles.sh
@@ -9,7 +9,7 @@ if [ ${PROCEED} == "N" ]; then
     echo "EXITING..."
     exit 1
 elif [ ${PROCEED} == "Y" ]; then
-    for file in .{bashrc,bash_profile,path,bash_prompt,exports,aliases,functions,extra}; do
+    for file in .{bashrc,bash_profile,path,bash_prompt,exports,aliases,functions,extra,inputrc}; do
         [ -r "$file" ] && [ -f "$file" ] && cp -v "./$file" "$HOME/$file";
         #[ -r "$file" ] && [ -f "$file" ] && echo "$file is here";
     done;
@@ -17,6 +17,6 @@ fi
 
 echo "FILES SYNCHRONIZED.  UPDATED:"
 
-for file in .{bashrc,bash_profile,path,bash_prompt,exports,aliases,functions,extra}; do
+for file in .{bashrc,bash_profile,path,bash_prompt,exports,aliases,functions,extra,inputrc}; do
     [ -r "$file" ] && [ -f "$file" ] && echo "$file";
 done;
